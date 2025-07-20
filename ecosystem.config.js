466f7fd46f78412417b18afbe2b9cf334aa2796a
kaikygr <kaikygomesribeiroof@gmail.com>
2025-07-20 14:33:12 -0400

    refactor: ajusta a configuração de memória máxima para 500mb e remove comentários desnecessários

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,17 +6,16 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: '500mb',
       env: {
         NODE_ENV: 'development',
       },
       env_production: {
         NODE_ENV: 'production',
       },
-      // Configuração para esperar o sinal 'ready'
       wait_ready: true,
-      listen_timeout: 10000, // Tempo máximo para esperar pelo sinal 'ready' (10 segundos)
-      kill_timeout: 5000, // Tempo para o PM2 esperar antes de forçar o encerramento (5 segundos)
+      listen_timeout: 10000,
+      kill_timeout: 5000,
     },
   ],
 };
